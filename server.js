cf16622b5f3806b8e77d23afb8fd46873e2d74fb
pcaridade/H <paulocaridade@gmail.com>
2017-11-25 09:45:29 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//set configurations (key, value)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//add "use" => middleware; "next"=when it's done
+app.use( (req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+/*
+app.use( (req, res, next) => {
+    res.render('maintenance.hbs');    
+});
+*/
+
+app.use(express.static(__dirname + '/public'));
+
+//
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
+
+    //res.send('<h1>Hellow Express!</h1>');
+    /*
+    res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    });
+    */
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+var server = app.listen(3000, () => {
+    var host = server.address().address
+    var port = server.address().port
+    console.log(`Server is up at ${host} port ${port}`);
+});
